5e9e405783935baee68e5401d905a122caff02f1
Anil <halfdeadentity@gmail.com>
2024-06-10 16:05:14 +0530

    correct error

--- src/App.js
+++ src/App.js
@@ -3,19 +3,19 @@ import Navbar from './navbar/Navbar'
 import MainPage from './pages/MainPage'
 import Footer from './footer/Footer'
 import { Routes, Route } from "react-router-dom";
-import Training from './training/Training';
+import Training from './training/Training.jsx';
 
 
 const App = () => {
   return (
-    <main>
+    <>
       <Navbar />
       <Routes>
         <Route path='/' element={<MainPage />} />
-        <Route path='training' element={<Training />} />
+        <Route path='/training' element={<Training />} />
       </Routes>
       <Footer />
-    </main>
+    </>
   )
 }
 
